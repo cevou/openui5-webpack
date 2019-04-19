072698f469549f5af6c576e193d45eb3b09fe705
Christoph Kraemer <chr.kraemer@sap.com>
2019-04-19 08:30:34 -0700

    Remove theme loaders as they are not used

--- packages/openui5-theme-base-loader/test/cases/add-dependency/index.js
+++
@@ -1,1 +0,0 @@
-require('less-loader!openui5-theme-base-loader!sap/ui/test/themes/sap_test/TestControl.less');
--- packages/openui5-theme-loader/test/cases/add-dependency/index.js
+++
@@ -1,3 +0,0 @@
-const Control = require('sap/ui/test/TestControl');
-const Control2 = require('sap/ui/test/TestControl2');
-const Control3 = require('sap/ui/test/TestControl3');
