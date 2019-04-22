fed392262259f97eb75876bfd050909287727c1e
Christoph Kraemer <chr.kraemer@sap.com>
2019-04-22 12:41:56 -0700

    Clean up resource module

--- packages/openui5-webpack-plugin/src/ResourceModuleFactory.js
+++ packages/openui5-webpack-plugin/src/ResourceModuleFactory.js
@@ -1,15 +1,11 @@
 const async = require('async');
-const Tapable = require('tapable').Tapable;
-const AsyncSeriesWaterfallHook = require('tapable').AsyncSeriesWaterfallHook;
+const { Tapable } = require('tapable');
 const ResourceModule = require('./ResourceModule');
 
 class ResourceModuleFactory extends Tapable {
   constructor(resolverFactory) {
     super();
-    this.hooks = {
-      beforeResolve: new AsyncSeriesWaterfallHook(['data']),
-      afterResolve: new AsyncSeriesWaterfallHook(['data']),
-    };
+
     this.resolverFactory = resolverFactory;
   }
 
@@ -18,72 +14,55 @@ class ResourceModuleFactory extends Tapable {
     const dependency = dependencies[0];
     const resolveOptions = data.resolveOptions;
 
-    this.hooks.beforeResolve.callAsync({
-      context: dependency.context,
-      modulePath: dependency.modulePath,
-      extensions: dependency.extensions,
-      libraries: dependency.libraries,
-      translations: dependency.translations,
-      locales: dependency.locales ? dependency.locales : dependency.translations,
-      failOnError: dependency.failOnError,
-    }, (err, result) => {
-      const context = result.context;
-      const modulePath = result.modulePath;
-      const extensions = result.extensions;
-      const libraries = result.libraries;
-      const translations = result.translations;
-      const locales = result.locales;
-      const failOnError = result.failOnError;
+    const context = dependency.context;
+    const modulePath = dependency.modulePath;
+    const extensions = dependency.extensions;
+    const libraries = dependency.libraries;
+    const translations = dependency.translations;
+    const locales = dependency.locales ? dependency.locales : dependency.translations;
+    const failOnError = dependency.failOnError;
 
-      const messagebundles = ['messagebundle.properties'];
-      translations.forEach((translation) => {
-        messagebundles.push(`messagebundle_${translation}.properties`);
+    const messagebundles = ['messagebundle.properties'];
+    translations.forEach((translation) => {
+      messagebundles.push(`messagebundle_${translation}.properties`);
+    });
+
+    const resources = [];
+    libraries.forEach((library) => {
+      const libSlash = library.replace(/\./g, '/');
+      messagebundles.forEach((messagebundle) => {
+        resources.push(`${libSlash}/${messagebundle}`);
       });
+    });
+    locales.forEach((translation) => {
+      resources.push(`sap/ui/core/cldr/${translation}.json`);
+    });
 
-      const resources = [];
-      libraries.forEach((library) => {
-        const libSlash = library.replace(/\./g, '/');
-        messagebundles.forEach((messagebundle) => {
-          resources.push(`${libSlash}/${messagebundle}`);
-        });
-      });
-      locales.forEach((translation) => {
-        resources.push(`sap/ui/core/cldr/${translation}.json`);
-      });
+    const normalResolver = this.resolverFactory.get('normal', resolveOptions || {});
 
-      const normalResolver = this.resolverFactory.get('normal', resolveOptions || {});
-
-      async.map(resources, (resource, callback) => {
-        normalResolver.resolve({}, dependency.context, resource, {}, (err) => {
-          if (err) {
-            callback();
-            return;
-          }
-          callback(null, resource);
-        });
-      }, (err, result) => {
+    async.map(resources, (resource, callback) => {
+      normalResolver.resolve({}, dependency.context, resource, {}, (err) => {
         if (err) {
-          callback(err);
+          callback();
           return;
         }
-
-        const resources = result.filter(resource => resource);
-
-        this.hooks.afterResolve.callAsync({
-          context,
-          modulePath,
-          extensions,
-          resources,
-          failOnError,
-        }, (err, result) => {
-          if (err) return callback(err);
-
-          // Ignored
-          if (!result) return callback();
-
-          return callback(null, new ResourceModule(result));
-        });
+        callback(null, resource);
       });
+    }, (err, result) => {
+      if (err) {
+        callback(err);
+        return;
+      }
+
+      const resources = result.filter(resource => resource);
+
+      return callback(null, new ResourceModule({
+        context,
+        modulePath,
+        extensions,
+        resources,
+        failOnError,
+      }));
     });
   }
 }
